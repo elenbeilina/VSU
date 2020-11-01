699aebd4861ab044203b66fd27f32d0693c19019
elenbeilina <elenbeilina>
2020-11-01 10:57:52 +0300

    - added environment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM openjdk:8-jdk-alpine
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
+
+ENV app_port=- \
+    server_oracle=- \
+    server_oracle_port=- \
+    oracle_db_name=- \
+    oracle_db_user=- \
+    oracle_db_password=-
+
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/aqualen/vsu/config/DataSourceConfig.java
+++ src/main/java/com/aqualen/vsu/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.aqualen.vsu.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,14 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${server.postrgre}")
+    private String server;
+
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/vsu");
+        dataSourceBuilder.url("jdbc:postgresql://"+ server + ":5432/vsu");
         dataSourceBuilder.username("vsu");
         dataSourceBuilder.password("pas");
         return dataSourceBuilder.build();
