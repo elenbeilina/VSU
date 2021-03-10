ddad2367502bd772533e9b030d7a37cf4c0ded13
elenbeilina <elenbeilina>
2021-03-10 09:07:04 +0300

    - added new layer to PlayerToPlayerPerformanceDifferencesLayer and PlayerDifferencesComparisonLayer

--- src/main/java/com/aqualen/vsu/trueSkill/layers/PlayerToPlayerPerformanceDifferencesLayer.java
+++ src/main/java/com/aqualen/vsu/trueSkill/layers/PlayerToPlayerPerformanceDifferencesLayer.java
@@ -1,11 +1,11 @@
 package com.aqualen.vsu.trueSkill.layers;
 
-import com.aqualen.vsu.trueSkill.factorGraphs.FactorGraphLayer;
-import com.aqualen.vsu.trueSkill.factorGraphs.variable.Variable;
-import com.aqualen.vsu.trueSkill.factors.GaussianWeightedSumFactor;
 import com.aqualen.vsu.trueSkill.GameInfo;
 import com.aqualen.vsu.trueSkill.GaussianDistribution;
 import com.aqualen.vsu.trueSkill.Player;
+import com.aqualen.vsu.trueSkill.factorGraphs.FactorGraphLayer;
+import com.aqualen.vsu.trueSkill.factorGraphs.variable.Variable;
+import com.aqualen.vsu.trueSkill.factors.GaussianWeightedSumFactor;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -19,22 +19,22 @@ public class PlayerToPlayerPerformanceDifferencesLayer extends
 
     @Override
     public void buildLayer(GameInfo gameInfo, List<Player> players) {
-        List<Variable<GaussianDistribution>> inputVariables = getInputVariables();
+        List<List<Variable<GaussianDistribution>>> inputVariables = getInputVariables();
 
         for (int i = 0; i < inputVariables.size() - 1; i++) {
-            Variable<GaussianDistribution> stronger = inputVariables.get(i);
-            Variable<GaussianDistribution> weaker = inputVariables.get(i + 1);
+            Variable<GaussianDistribution> stronger = inputVariables.get(i).get(0);
+            Variable<GaussianDistribution> weaker = inputVariables.get(i + 1).get(0);
 
             Variable<GaussianDistribution> currentDifference = createOutputVariable();
             addLayerFactor(createPlayerPerformanceToDifferenceFactor(stronger, weaker, currentDifference));
 
             // REVIEW: Does it make sense to have groups of one?
-            addToOutputVariables(currentDifference);
+            addToOutputVariables(List.of(currentDifference));
         }
     }
 
     private Variable<GaussianDistribution> createOutputVariable() {
-        return new Variable<>("Players performance difference", GAUSSIAN_LINE);
+        return new Variable<>(GAUSSIAN_LINE, "Players performance difference");
     }
 
     private GaussianWeightedSumFactor createPlayerPerformanceToDifferenceFactor(
